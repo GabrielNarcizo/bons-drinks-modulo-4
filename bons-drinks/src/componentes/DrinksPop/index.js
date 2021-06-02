46b2197725649518988e38153caacc34cf9bdb53
GabrielNarcizo <eligcan@hotmail.com>
2021-06-02 17:34:38 -0300

    Adicionando Hook

---
+++ bons-drinks/src/componentes/DrinksPop/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DrinksPop = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default DrinksPop
---
+++ bons-drinks/src/componentes/Hooks/fetch.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react'
+
+const Fetch = () => {
+    const [drinks, setDrinks] = useState([])
+
+    useEffect(() =>{
+        const getDrinks = async () => {
+            const response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php");
+            const data = await response.json()
+            setDrinks(data.drinks)
+        }
+        getDrinks()
+    },[])
+    return drinks
+}
+
+export default Fetch;
